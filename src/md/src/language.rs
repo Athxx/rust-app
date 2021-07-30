63cba16a1bba24e90c18eca1aeb0f340fbf15f2a
X <x@xktech.com>
2021-07-30 17:36:12 +0800

    init

---
+++ src/md/src/language.rs
@@ -0,0 +1,8 @@
+const LANG_EN: str = "english";
+const LANG_ZHS: str = "english";
+const LANG_ZHT: str = "english";
+const LANG_AR: str = "Arabic";
+const LANG_KO: str = "";
+const LANG_JA: str = "";
+const LANG_RU: str = "";
+const LANG_VN: str = "";
